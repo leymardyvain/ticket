d0cb2af0ba830b8f50f9a73744a4b22d9c8005af
leymardyvain <leymardyvain@gmail.com>
2025-10-23 17:33:29 +0100

    update solution dialog

--- src/main/frontend/src/views/ticket/DisplaySolutionDialog.jsx
+++ src/main/frontend/src/views/ticket/DisplaySolutionDialog.jsx
@@ -1,7 +1,7 @@
-import { Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Grid, IconButton, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip, Typography } from "@mui/material";
+import { Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Grid, IconButton, Stack, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { getCountFichier, getDownloadFileName, getFichierByIDTicket } from "../../api/APIfichier";
-import { IconCircleX, IconDownload, IconEye, IconPlaystationX } from "@tabler/icons-react";
+import { IconCheckbox, IconCircleX, IconDownload, IconEye, IconPlaystationX } from "@tabler/icons-react";
 import { development, production } from 'api/connexion';
 import { format } from "date-fns";
 import { getSolutionBySuiviTicket } from "../../api/APIsolution";
@@ -19,16 +19,22 @@ export default function DisplaySolutionDialog({ open, setOpen, data }) {
 
     const [listeSolution, setListeSolution] = useState([]);
 
+    const [selectedSolution, setSelectedSolution] = useState({});
+
     const handleClose = () => {
         setOpen(false);
     };
 
+    const Displaydetails = (row) => {
+        setSelectedSolution(() => row);
+    };
+
     const fetchDataSolutionTicket = async () => {
         try {
-          /*  const response = await getSolutionBySuiviTicket(data?.id_suivi_Ticket);
-            setListeSolution(response.data); */
+            /*  const response = await getSolutionBySuiviTicket(data?.id_suivi_Ticket);
+              setListeSolution(response.data); */
             if (data?.id_suivi_Ticket) {
-                console.log('id solution ',data?.id_suivi_Ticket )
+                console.log('id solution ', data?.id_suivi_Ticket)
                 const response = await getSolutionBySuiviTicket(data?.id_suivi_Ticket);
                 setListeSolution(response.data);
             }
@@ -40,6 +46,8 @@ export default function DisplaySolutionDialog({ open, setOpen, data }) {
     React.useEffect(() => {
         fetchDataSolutionTicket();
     }, [open]);
+
+    const isDataEmpty = Object.keys(selectedSolution).length === 0;
 
     return (
         <React.Fragment>
@@ -70,40 +78,82 @@ export default function DisplaySolutionDialog({ open, setOpen, data }) {
                 </IconButton>
 
                 <DialogContent dividers>
-
-                    {listeSolution.length > 0 ?
-                        <TableContainer>
-                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell align="left">ID</TableCell>
-                                        <TableCell align="left">Type equipement</TableCell>
-                                        <TableCell align="left">Marque equipement</TableCell>
-                                        <TableCell align="left">Autre equipement</TableCell>
-                                        <TableCell align="left">Description solution</TableCell>
-                                        <TableCell align="left">Remarque</TableCell>
-                                        <TableCell align="left">Localisation</TableCell>
-                                        <TableCell align="left">Date</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {listeSolution.map((row) => (
-                                        <TableRow key={row.id_solution}>
-                                            <TableCell align="left">{row.id_solution}</TableCell>
-                                            <TableCell align="left">{row.type_equipement !== " " ? row.type_equipement : "N/A"}</TableCell>
-                                            <TableCell align="left">{row.marque_equipement !== " " ? row.marque_equipement : "N/A"}</TableCell>
-                                            <TableCell align="left">{row.autre_equipement !== " " ? row.autre_equipement : "N/A"}</TableCell>
-                                            <TableCell align="left">{row.description_solution !== " " ? row.description_solution : "N/A"}</TableCell>
-                                            <TableCell align="left">{row.remarque !== " " ? row.remarque : "N/A"}</TableCell>
-                                            <TableCell align="left">{row.localisation !== " " ? row.localisation : "N/A"}</TableCell>
-                                            <TableCell align="left">{format(row.date_solution, "dd-MM-yyyy HH:mm")}</TableCell>
+                    <Stack spacing={4}>
+                        {listeSolution.length > 0 &&
+                            <TableContainer>
+                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="left">ID</TableCell>
+                                            <TableCell align="left">Type equipement</TableCell>
+                                            <TableCell align="left">Marque equipement</TableCell>
+                                            <TableCell align="left">Autre equipement</TableCell>
+                                            <TableCell align="left">Localisation</TableCell>
+                                            <TableCell align="left">Date</TableCell>
+                                            <TableCell align="left">Action</TableCell>
                                         </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                   : <Typography sx={{color:'red'}} component="span" variant="body2">Aucune solution pour ce ticket</Typography> }
+                                    </TableHead>
+                                    <TableBody>
+                                        {listeSolution.map((row) => (
+                                            <TableRow key={row.id_solution}>
+                                                <TableCell align="left">{row.id_solution}</TableCell>
+                                                <TableCell align="left">{row.type_equipement !== "" ? row.type_equipement : "N/A"}</TableCell>
+                                                <TableCell align="left">{row.marque_equipement !== "" ? row.marque_equipement : "N/A"}</TableCell>
+                                                <TableCell align="left">{row.autre_equipement !== "" ? row.autre_equipement : "N/A"}</TableCell>
+                                                <TableCell align="left">{row.localisation !== "" ? row.localisation : "N/A"}</TableCell>
+                                                <TableCell align="left">{format(row.date_solution, "dd-MM-yyyy HH:mm")}</TableCell>
+                                                <TableCell align='left'>
+                                                    <Tooltip title="Afficher">
+                                                        <IconButton aria-label="afficher"
+                                                            size="large"
+                                                            sx={{ bgcolor: '#f1e9ddff', color: "#486de7ff" }}
+                                                            onClick={() => Displaydetails(row)}>
+                                                            <IconEye fontSize="inherit" style={{ strokeWidth: "3" }} />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        }
 
+                        {!isDataEmpty && <Grid container spacing={2}>
+                            <Grid size={6}>
+                                <TextField label="Type equipement" value={selectedSolution.type_equipement} fullWidth inputProps={{
+                                    readOnly: true,
+                                }} InputLabelProps={{ shrink: true }} />
+                            </Grid>
+                            {selectedSolution.autre_equipement && <Grid size={6}>
+                                <TextField label="Autre equipement" value={selectedSolution.autre_equipement} fullWidth inputProps={{
+                                    readOnly: true,
+                                }} InputLabelProps={{ shrink: true }} />
+                            </Grid>}
+                            <Grid size={6}>
+                                <TextField label="Marque equipement" value={selectedSolution.marque_equipement} fullWidth inputProps={{
+                                    readOnly: true,
+                                }} InputLabelProps={{ shrink: true }} />
+                            </Grid>
+                            <Grid size={6}>
+                                <TextField label="Emplacement" value={selectedSolution.localisation} fullWidth inputProps={{
+                                    readOnly: true,
+                                }} InputLabelProps={{ shrink: true }} />
+                            </Grid>
+
+                            <Grid size={12}>
+                                <TextField label="Description solution" multiline rows={5} value={selectedSolution.description_solution} fullWidth inputProps={{
+                                    readOnly: true,
+                                }} InputLabelProps={{ shrink: true }} />
+                            </Grid>
+                            <Grid size={12}>
+                                <TextField label="Remarque" multiline rows={3} value={selectedSolution.remarque} fullWidth inputProps={{
+                                    readOnly: true,
+                                }} InputLabelProps={{ shrink: true }} />
+                            </Grid>
+                        </Grid>
+                        }
+                    </Stack>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Fermer</Button>
